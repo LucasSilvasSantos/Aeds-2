4c126d0ac9ee6c0d2575b48ab96cd3d00ea0df40
LucasSilvasSantos <lucsantos820@gmail.com>
2025-09-02 08:06:06 -0300

    p1

--- tp2/Algebraboleana.java
+++ tp2/Algebraboleana.java
@@ -1,75 +1,56 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Algebraboleana {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        while(sc.hasNext()) {
-            // Lê o número de entradas
-            int n = sc.nextInt();
-            
-            // Lê os valores binários
-            boolean[] valores = new boolean[n];
-            for(int i = 0; i < n; i++) {
-                valores[i] = sc.nextInt() == 1;
+
+	public static boolean getAnd(String str, int valores[], int i) {
+		boolean resp = false;
+
+		for (i=0; i < str.length(); i++) {
+			if (str.charAt(i) == 'n' && str.charAt(i+1) == 'o' && str.charAt(i+2) == 't') {
+
+			}
+		}
+
+		return resp;
+	}
+
+	public static boolean valorBooleano(String str, int valores[]) {
+		boolean resp = false;
+		int cont = 0;
+
+		for (int i=0; i < str.length(); i++) {
+			if (str.charAt(i) == 'a' && str.charAt(i+1) == 'n' && str.charAt(i+2) == 'd') {
+				resp = getAnd(str, valores, i+4);
+			} 
+			else if (str.charAt(i) == 'o' && str.charAt(i+1) == 'r') {
+				cont++;
+			}
+			else if (str.charAt(i) == 'n' && ){
+                
             }
-            
-            // Lê a expressão (resto da linha)
-            sc.nextLine(); // Limpa o buffer
-            String expressao = sc.nextLine();
-            
-            // Avalia a expressão
-            boolean resultado = avaliarExpressao(expressao, valores);
-            
-            // Imprime resultado
-            System.out.println(resultado ? "SIM" : "NAO");
-        }
-        sc.close();
-    }
-    
-    public static boolean avaliarExpressao(String expressao, boolean[] valores) {
-        // Substitui as letras pelos valores
-        for(int i = 0; i < valores.length; i++) {
-            char letra = (char)('A' + i);
-            expressao = expressao.replace(String.valueOf(letra), 
-                                       String.valueOf(valores[i]));
-        }
-        
-        // Remove espaços e parênteses desnecessários
-        expressao = expressao.replaceAll("\\s+", "");
-        
-        // Avalia NOT
-        while(expressao.contains("not")) {
-            int index = expressao.indexOf("not");
-            boolean valor = Boolean.parseBoolean(
-                expressao.substring(index + 3, index + 8));
-            expressao = expressao.substring(0, index) + (!valor) + 
-                       expressao.substring(index + 8);
-        }
-        
-        // Avalia AND
-        while(expressao.contains("and")) {
-            int index = expressao.indexOf("and");
-            boolean valor1 = Boolean.parseBoolean(
-                expressao.substring(index - 5, index));
-            boolean valor2 = Boolean.parseBoolean(
-                expressao.substring(index + 3, index + 8));
-            expressao = expressao.substring(0, index - 5) + 
-                       (valor1 && valor2) + 
-                       expressao.substring(index + 8);
-        }
-        
-        // Avalia OR
-        while(expressao.contains("or")) {
-            int index = expressao.indexOf("or");
-            boolean valor1 = Boolean.parseBoolean(
-                expressao.substring(index - 5, index));
-            boolean valor2 = Boolean.parseBoolean(
-                expressao.substring(index + 2, index + 7));
-            expressao = expressao.substring(0, index - 5) + 
-                       (valor1 || valor2) + 
-                       expressao.substring(index + 7);
-        }
-        
-        return Boolean.parseBoolean(expressao);
-    }
+		}
+
+		return resp;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		while(n > 0) {
+			int valores[] = new int[n];
+			for (int i=0; i < n; i++) {
+				int temp = sc.nextInt();
+				valores[i] = temp;
+			}
+			String str = sc.nextLine();
+
+			System.out.println((valorBooleano(str, valores)) ? "1" : "0");
+
+			n = sc.nextInt();
+		}
+
+
+		sc.close();
+	}
 }
