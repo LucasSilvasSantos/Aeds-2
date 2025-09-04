de2d271ad308ae29b525c519c7ab2fb2e17f0a65
LucasSilvasSantos <lucsantos820@gmail.com>
2025-09-04 18:36:08 -0300

    att

--- oficina/exec1.c
+++ oficina/exec1.c
@@ -57,31 +57,31 @@ Tarefa desempilharEmergencia(PilhaEmergencia* p){
         Tarefa desempilhada;
         desempilhada = p->pilha[p->n];
       p->n--; //desinfelirei o ultimo elemento
-     return p->pilha[p->n];// retornando o ultimo elemento
+     return p->pilha[p->n];// retornando o ultimo elemento ou seja  valor ja desempilhado
 
 
-    }  // TO DO: IMPLEMENTAR // desempilhar uma tarefa da pilha
+    }  
 }
 
 void enfileirarPeriodica(FilaPeriodica* f, Tarefa t){
-    if ((f->ultimo+1) % MAX_PERIODICA == f->primeiro){
+    if ((f->ultimo+1) % MAX_PERIODICA == f->primeiro){ //confiro se todos os elementos estao prenchidos  e faco com que o ultimo volte para o inicio
       printf("Fila cheia\n");
       return;
     }
     else {  
       f->fila[(f->ultimo+1) % MAX_PERIODICA]=t; // enfilerei a tarefa depois da ultima posicao,e para nao sobrepor eu faco a divisao do max periodica
- f->ultimo    
+      f->ultimo = (f->ultimo+1) % MAX_PERIODICA; // atualizando a posicao do ultimo    
     }
-    
-    
-    
-    // TO DO: IMPLEMENTAR
-    // enfilierar um elemento na fila
 }
 
 Tarefa desenfileirarPeriodica(FilaPeriodica* f){
-    // TO DO: IMPLEMENTAR
-    // desinfileirar uma tarefa da fila
+ if(f->primeiro == f->ultimo){ //se o primeiro foi igual ao ultimo, a fila esta vazia
+ printf("fila vazia");
+ }
+ Tarefa t = f->fila[f->primeiro]; // pegando o elemento que esta na posicao do primeiro
+f->primeiro =(f->primeiro +1) % MAX_PERIODICA; //atualizo a posicao do primeiro elemento
+
+
 }
 
 void inserirBackground(ListaBackground* l, Tarefa t){
