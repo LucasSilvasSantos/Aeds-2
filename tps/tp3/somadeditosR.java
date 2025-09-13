d506389add5d468da64ba8811cde9f25b3dec5c2
LucasSilvasSantos <lucsantos820@gmail.com>
2025-09-13 12:10:33 -0300

    att

--- tps/tp3/somadeditosR.java
+++ tps/tp3/somadeditosR.java
@@ -1,9 +1,30 @@
-
+import java.util.Scanner;
 public class somadeditosR{
+  public static int stringParaInt(String entrada) {
+    int numero = 0;
+    for (int i = 0; i < entrada.length(); i++) {
+        char c = entrada.charAt(i);
+        int digito = c - '0';       // converte caractere para número atraves da tabaela ASCII 
+        numero = numero * 10 + digito; 
+    }
+    return numero;
+}
+
+  
   public static void main(String[] args) {
-      while(1){
-
-
+      Scanner sc =new Scanner (System.in);
+      while (true) {
+     String entrada =sc.nextLine();
+       int n ,soma = 0;
+       n = stringParaInt(entrada);
+     while( n > 0) {
+     soma+= n%10; // pegando o resto da divisão  e armazenando na variavel soma
+     n/=10; // dividindo o numero por 10  com  objetivo de remover o ultimo digito
+     }
+        if (entrada.equals("FIM")) {
+    break;
+}        
+       System.out.println(soma);
       }
 
 
