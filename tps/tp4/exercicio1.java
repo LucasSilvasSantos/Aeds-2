4288eee1a7b1540183116f885161670a16f0ad91
LucasSilvasSantos <lucsantos820@gmail.com>
2025-10-07 20:50:49 -0300

    att tp4

---
+++ tps/tp4/exercicio1.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class exercicio1 {
+
+    public static class Game {
+
+        public int id;
+        public String name;
+        public String realeasedate;
+        public int estimatedoweners;
+        public float price;
+        public List<String> supportedLnaguages = new ArrayList<>();
+        public int metacirticScore;
+        public float userScore;
+        public int achievements;
+        public List<String> publishers = new ArrayList<>();
+        public List<String> developers = new ArrayList<>();
+        public List<String> categories = new ArrayList<>();
+        public List<String> genres = new ArrayList<>();
+        public List<String> tags = new ArrayList<>();
+    }
+
+}
