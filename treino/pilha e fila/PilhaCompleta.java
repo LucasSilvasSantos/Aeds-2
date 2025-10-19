96fba0bfddee4874979ef8fae561ea962f3ef5b0
LucasSilvasSantos <lucsantos820@gmail.com>
2025-10-19 20:28:29 -0300

    att

--- treino/pilha e fila/PilhaCompleta.java
+++ treino/pilha e fila/PilhaCompleta.java
@@ -1,11 +1,15 @@
+
+import java.util.Scanner;
+
 class Celula {
+
     public int elemento;
     public Celula prox;
-    
+
     public Celula() {
         this(0);
     }
-    
+
     public Celula(int x) {
         this.elemento = x;
         this.prox = null;
@@ -13,38 +17,55 @@ class Celula {
 }
 
 class Pilha {
+
     private Celula topo;
-    
+
     public Pilha() {
         topo = null;
     }
 
     public void inserir(int x) {
-      Celula tmp = new Celula(x);
-      tmp.prox= topo;
-      topo = tmp;
-      tmp = null;
+        Celula tmp = new Celula(x); // crio uma celula temporaria para armazenar um elemento que vai ser inserido 
+        tmp.prox = topo;   // faço a proxima celula temporaria apontar para topo 
+        topo = tmp; // faço com que a celula topo aponte para a celula temporaria 
+        tmp = null; // removo a celula temporaria 
     }
+
     public int remover() {
-     if (topo != null ){
-       int elemento = topo.elemento; // crio uma celula  para armazenar o elemento que  vai ser removido e esta no topo 
-       Celula tmp = topo ; // crio uma celula temporararia  para apontar para o elemento de  topo  
-       topo = topo.prox; //aponto para o novo topo 
-       tmp.prox = null;// desconecto o elemento da pilha 
-       tmp = null; // limpo a celula temporaria 
-        return elemento;
-     }
+        if (topo != null) {
+            int elemento = topo.elemento; // crio uma celula  para armazenar o elemento que  vai ser removido e esta no topo 
+            Celula tmp = topo; // crio uma celula temporararia  para apontar para o elemento de  topo  
+            topo = topo.prox; //aponto para o novo topo 
+            tmp.prox = null;// desconecto o elemento da pilha 
+            tmp = null; // limpo a celula temporaria 
+            return elemento;
+        }
+        return -1;
     }
+
     public void mostrar() {
- System.out.print("[");
-  for(Celula i = topo ; i !=null ; i=i.prox){
-     System.out.print(i.elemento + "");
+        System.out.print("[");
+        for (Celula i = topo; i != null; i = i.prox) {
+            System.out.print(i.elemento + "");
 
-  }
-  System.out.println("]");
+        }
+        System.out.println("]");
 
+    }
+}
 
+public class PilhaCompleta {
 
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pilha pilha = new Pilha();
+        System.out.println(" digite o numero que vc quer inserir na pilha :      ( para sair digite -1)");
+        int numero = sc.nextInt();
+        while (numero != -1) {
+            pilha.inserir(numero);
+            numero = sc.nextInt();
+        }
+        System.out.println(" elementos da pilha : ");
+        pilha.mostrar();
     }
 }
