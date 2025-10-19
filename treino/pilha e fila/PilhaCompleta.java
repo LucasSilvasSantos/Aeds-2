c43fcd4e9e96c9e420524f32564fcdeaf0469339
LucasSilvasSantos <lucsantos820@gmail.com>
2025-10-19 10:56:04 -0300

    att

--- treino/pilha e fila/PilhaCompleta.java
+++ treino/pilha e fila/PilhaCompleta.java
@@ -20,7 +20,31 @@ class Pilha {
     }
 
     public void inserir(int x) {
+      Celula tmp = new Celula(x);
+      tmp.prox= topo;
+      topo = tmp;
+      tmp = null;
     }
-    public int remover() throws Exception {}
-    public void mostrar() {}
+    public int remover() {
+     if (topo != null ){
+       int elemento = topo.elemento; // crio uma celula  para armazenar o elemento que  vai ser removido e esta no topo 
+       Celula tmp = topo ; // crio uma celula temporararia  para apontar para o elemento de  topo  
+       topo = topo.prox; //aponto para o novo topo 
+       tmp.prox = null;// desconecto o elemento da pilha 
+       tmp = null; // limpo a celula temporaria 
+        return elemento;
+     }
+    }
+    public void mostrar() {
+ System.out.print("[");
+  for(Celula i = topo ; i !=null ; i=i.prox){
+     System.out.print(i.elemento + "");
+
+  }
+  System.out.println("]");
+
+
+
+
+    }
 }
