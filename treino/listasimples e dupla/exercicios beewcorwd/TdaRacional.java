d84d0c7ffc95431e2dea1a8a1e58689fa05a01d5
Lucas Silva Santos <lucsantos820@gmail.com>
2025-10-30 19:32:11 +0000

    att

---
+++ treino/listasimples e dupla/exercicios beewcorwd/TdaRacional.java
@@ -0,0 +1,72 @@
+package exercicios beewcorwd;
+import java.io.IOException;
+import java.util.Scanner;
+public class TdaRacional {
+    
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int contador =0;
+         for (int i =0; i < contador; i ++){
+              int n1  = sc.nextInt;
+              String simbolos = sc.next;
+               int n2 =sc.nextInt ;
+              int d2= sc.nextInt;
+              int d1 =sc.nextInt;
+               switch (simbolos)
+                 case ("+"){
+                     (n1*d2 + n2*d1)/d1*d2; 
+                     
+                     
+                 }
+                  case ("-"){
+                     (n1*d2 - n2*d1)/(d1*d2); 
+                     
+                     
+                 }
+                  case ("*"){
+                     (n1*n2)/(d1*d2); 
+                     
+                     
+                 }
+                  case ("/"){
+                     (n1/d2)/(n2*d1); 
+                     
+                     
+                 }
+               
+              int n2 =sc.nextInt ;
+              int d2= sc.nextInt;
+              int d1 =sc.nextInt;
+             
+             
+             
+             
+         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+ 
+    }
+ 
+}
+}
