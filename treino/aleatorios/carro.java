396a0e8735b4eaaaa004c5cd35d9d98493523505
Lucas Silva Santos <61788@emater.mg.gov.br>
2026-02-11 16:50:51 -0300

    ataulização

---
+++ treino/aleatorios/carro.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+private class Carro {
+   String placa;
+   String modelo;
+   int ano;
+   String cor;
+   float tabela_fipe;
+
+   
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
