e67b0a184c0b34e4e4e459b3bd7a026d9a30be15
LucasSilvasSantos <lucsantos820@gmail.com>
2026-02-19 20:09:58 -0300

    atualização

--- treino/aleatorios/nameage.java
+++ treino/aleatorios/nameage.java
@@ -1,32 +1,21 @@
 
 public class nameage {
-     private class Pessoa { 
-          String name;
-          int age;
+      
+
+
+
+      }
+
+
+
+
+     
 
 
 
 
 
-
-
-
-     } 
-
-
-
-
-
-    public static void main(String[] args) {
-        int [] age = {20,30,40,50};
-        String [] name = {"lucas","maria","joao","ana"};
     
-
-
-
-
-    }
-}
 
 
 
