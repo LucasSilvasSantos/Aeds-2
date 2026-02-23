f55a49dc9c113db75cc57496a581da6fa46450a0
Lucas Silva Santos <61788@emater.mg.gov.br>
2026-02-23 15:58:59 -0300

    atualizando codigo palindromo

--- lab/palindromo.java
+++ lab/palindromo.java
@@ -2,38 +2,32 @@ import java.util.Scanner;
 
 public class palindromo {
     public static boolean isPalindromo( String word ){
-         for (int i =0; i< word.length()/2; i++){
-                  if (word.charAt(i) != word.charAt(word.length()-1-i)){
+         for (int i = 0; i < word.length() / 2; i++){
+                  if (word.charAt(i) != word.charAt(word.length() - 1 - i)){
                          return false;
                   }
 
-                  else {
-                        return true;
-                  }
+         }
+         return true;
+    }
 
-
+         public static void main(String[] args){
+             Scanner sc = new Scanner(System.in);
+             while (true) {
+                 String word = sc.nextLine();
+                 if (word.equals("FIM")) {
+                     break;
+                 }
+                 boolean res = isPalindromo(word);
+                 if (res) {
+                     System.out.println("SIM");
+                 } else {
+                     System.out.println("NAO");
+                 }
+             }
+             sc.close();
 
          }
-    }
-     public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-        String word = sc.nextLine();
-        boolean res = isPalindromo(word);s
-        if (res){
-            System.out.println("SIM");
-        }
-        else {
-            System.out.println("NÃO");
-        }
-           if(word.equals("FIM")){
-               return;
-           }
-
-
-
-
-
-     }
 
 
 }
