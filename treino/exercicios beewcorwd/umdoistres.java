156e64c4710ed84c1cc8cc8f17113116dbad5832
Lucas Silva Santos <61788@emater.mg.gov.br>
2026-03-12 16:49:20 -0300

    att

---
+++ treino/exercicios beewcorwd/umdoistres.java
@@ -0,0 +1,33 @@
+import java.util.Scanner 
+
+public class umdoistres{
+public static void main ( String [] args ){
+ Scanner sc = new Scanner (System.in);
+
+ int prohibited = sc.nextInt();
+    for( int i =0; i < prohibited; i ++){
+     String word = sc.nextLine;
+       if(word.length == 3) {
+        for(int confere=0 ; confere<= word.length; confere ++){
+         if(word.charAt(i) == "o" && "n" || )       
+        }
+       }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+}
