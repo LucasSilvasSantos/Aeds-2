7db5953459d28dfcd9eed4dff4dac39f1938c1ae
LucasSilvasSantos <lucsantos820@gmail.com>
2026-04-28 18:40:13 -0300

    Atualização sobre a fila flexivel

--- treino/Bases/pilha e fila/tadflexivelfila.java
+++ treino/Bases/pilha e fila/tadflexivelfila.java
@@ -26,18 +26,45 @@ class Fila {
   public void inserir (int x) {
      ultimo.prox = new Celula (x);
       ultimo = ultimo.prox;
+      
+  }
+
+  public int remover () {
+ if(primeiro == ultimo){
+     throw new RuntimeException ("Fila Vazia");
+ }
+Celula tmp =primeiro;
+ primeiro = primeiro.prox;
+  tmp.prox =null;
+  tmp= null;
+  int elemento = primeiro.elemento;
+
+  return elemento;
 
   }
 
-  public int remover () TR{
+
+public int somar () {
+   int somar = 0 ;
+ if( primeiro == ultimo){
+     throw new RuntimeException ("Fila Vazia");
+ }
+   for( int i = primeiro.prox.elemento; i != ultimo.elemento; i = i. prox.elemento){
+       somar += i;
+   }
 
 
 
 
+}
+
+  public void mostrar () {
+ for(Celula i = primeiro.prox; i != null; i = i.prox){
+        System.out.print(i.elemento + " ");
+ }
 
 
   }
-
 
 }
 
