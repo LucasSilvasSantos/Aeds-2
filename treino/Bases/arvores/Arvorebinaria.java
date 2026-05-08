e5c8bf695b823ebf09d5dd6badaf67a4cb8bc265
Lucas Silva Santos <lucsantos820@gmail.com>
2026-05-08 14:02:17 -0300

    metodos arvore completo

--- treino/Bases/arvores/Arvorebinaria.java
+++ treino/Bases/arvores/Arvorebinaria.java
@@ -49,8 +49,42 @@ else throw new Exception("Erro!"); // se o elemento já existe na árvore, lanç
 
 void remover ( int x ) throws Exception {
  if (raiz == null) throw new Exception("Erro!"); // se a raiz for null, lança uma exceção
- else remover(x, raiz); // caso contrário, chama o método auxiliar para remover o elemento da árvore
+ else raiz = remover(x, raiz); // caso contrário, chama o método auxiliar para remover o elemento da árvore
 
 }
 
+No remover ( int x , No i ) throws Exception {
+ if (i == null) throw new Exception("Erro!"); // se o nó atual for null, lança uma exceção
+ else if (x < i.elemento) i.esq = remover(x, i.esq); // se o elemento a ser removido for menor que o elemento do nó atual, chama recursivamente para o filho esquerdo
+ else if (x > i.elemento) i.dir = remover(x, i.dir); // se o elemento a ser removido for maior que o elemento do nó atual, chama recursivamente para o filho direito
+ else if (i.dir == null) i = i.esq; // se o nó a ser removido não tiver filho direito, substitui o nó pelo seu filho esquerdo
+ else if (i.esq == null) i = i.dir; // se o nó a ser removido não tiver filho esquerdo, substitui o nó pelo seu filho direito
+ else i.esq = maiorEsq(i, i.esq); // caso contrário, substitui o nó pelo maior elemento da subárvore esquerda
+ return i; // retorna o nó atualizado
+}
 
+
+/* CONSULTA */
+
+ boolean pesquisar ( int x ){
+ return pesquisar(x, raiz); // chama o método auxiliar para pesquisar o elemento na árvore, começando pela raiz 
+ }
+
+boolean pesquisar ( int x , No i ) { 
+boolean resp; 
+ if (i == null) resp= false; // se o nó atual for null, o elemento não foi encontrado, retorna false
+ else if (x == i.elemento) resp= true; // se o elemento do nó atual for igual ao elemento a ser pesquisado, retorna true
+ else if (x < i.elemento) resp= pesquisar(x, i.esq); // se o elemento a ser pesquisado for menor que o elemento do nó atual, chama recursivamente para o filho esquerdo
+ else resp= pesquisar(x, i.dir); // se o elemento a ser pesquisado for maior que o elemento do nó atual, chama recursivamente para o filho direito
+ return resp;
+}
+  /* Altura da árvore */
+
+int altura ( ){
+return altura (raiz); // chama o método auxiliar para calcular a altura da árvore, começando pela raiz
+}
+
+
+
+
+}
