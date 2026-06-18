a503a8ab130c298a19ba13053ae6bc90f8956c34
LucasSilvasSantos <lucsantos820@gmail.com>
2026-06-18 07:35:13 -0300

    atualização

---
+++ treino/exercicios beewcorwd/4-ESTRUTURAS E BIBLIOTECAS/Marmore.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+
+public class Marmore {
+static int lerInt() thrown IOException {}
+  int c;
+   while ((c = System.in.read()) < '0' || c > '9'); // le apenas um caractere, ignorando tudo que não for dígito
+    
+
+}
